dfabc3cd68777557f789d8d1af70f4820f6914e2
Guest User <Guest@Mac-mini-7.local>
2023-10-24 10:33:08 +0200

    Init project

---
+++ NearestATM/NearestATMApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearestATMApp.swift
+//  NearestATM
+//
+//  Created by Guest User on 24/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct NearestATMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
