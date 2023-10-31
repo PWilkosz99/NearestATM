eeef858e21f85820c308c6983221e5f2c020a8dc
Piotr Wilkosz <pwilkosz99@gmail.com>
2023-10-31 16:50:35 +0100

    Save cooridinates

--- NearestATM/Models/ATM.swift
+++ NearestATM/Models/ATM.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CoreLocation
 
 struct ATM : Identifiable{
     let id: UUID
@@ -13,14 +14,16 @@ struct ATM : Identifiable{
     var city: String
     var address: String
     var cash_in: Bool
+    var location: CLLocationCoordinate2D
     var theme: Theme
 
-init(id: UUID = UUID(), brand: String, city: String, address: String, cash_in: Bool, theme: Theme){
+    init(id: UUID = UUID(), brand: String, city: String, address: String, cash_in: Bool, location: CLLocationCoordinate2D, theme: Theme){
     self.id = id
     self.brand = brand
     self.city = city
     self.address = address
     self.cash_in = cash_in
+    self.location = location
     self.theme = theme
     }
 }
@@ -32,18 +35,21 @@ extension ATM{
             city: "Kraków",
             address: "Czarnowiejska 10",
             cash_in: false,
+            location: CLLocationCoordinate2D(latitude: 50.0647, longitude: 19.9450),
             theme: .bubblegum
         ),
         ATM(brand: "Bank Pekao",
             city: "Kraków",
             address: "Czarnowiejska 20",
             cash_in: false,
+            location: CLLocationCoordinate2D(latitude: 50.0647, longitude: 19.9450),
             theme: .buttercup
         ),
         ATM(brand: "Euronet",
             city: "Kraków",
             address: "Czarnowiejska 30",
             cash_in: true,
+            location: CLLocationCoordinate2D(latitude: 50.0647, longitude: 19.9450),
             theme: .indigo
         )
     ]
