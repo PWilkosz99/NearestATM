743d6640c2245923694f43df5fbe74044ce2ee80
Piotr Wilkosz <pwilkosz99@gmail.com>
2023-11-20 21:42:09 +0100

    Redefine ATM parameters

--- NearestATM/Models/ATM.swift
+++ NearestATM/Models/ATM.swift
@@ -11,20 +11,31 @@ import CoreLocation
 struct ATM : Identifiable{
     let id: UUID
     var brand: String
+    var cash_in: Bool
+    
+    var location: CLLocationCoordinate2D
     var city: String
     var address: String
-    var cash_in: Bool
-    var location: CLLocationCoordinate2D
+    
+    var opening_hours: String?
+    var fee: String?
+    var charge: String?
+    var wheelchair: String?
+    
     var theme: Theme
 
-    init(id: UUID = UUID(), brand: String, city: String, address: String, cash_in: Bool, location: CLLocationCoordinate2D, theme: Theme){
-    self.id = id
-    self.brand = brand
-    self.city = city
-    self.address = address
-    self.cash_in = cash_in
-    self.location = location
-    self.theme = theme
+    internal init(id: UUID = UUID(), brand: String, cash_in: Bool, location: CLLocationCoordinate2D, city: String, address: String, opening_hours: String? = nil, fee: String? = nil, charge: String? = nil, wheelchair: String? = nil, theme: Theme) {
+        self.id = id
+        self.brand = brand
+        self.cash_in = cash_in
+        self.location = location
+        self.city = city
+        self.address = address
+        self.opening_hours = opening_hours
+        self.fee = fee
+        self.charge = charge
+        self.wheelchair = wheelchair
+        self.theme = theme
     }
 }
 
@@ -32,24 +43,24 @@ extension ATM{
     static let sampleData: [ATM] =
     [
         ATM(brand: "Bank PKO",
-            city: "Kraków",
-            address: "Czarnowiejska 10",
             cash_in: false,
             location: CLLocationCoordinate2D(latitude: 50.0647, longitude: 19.9450),
+            city: "Kraków",
+            address: "Czarnowiejska 10",
             theme: .bubblegum
         ),
         ATM(brand: "Bank Pekao",
-            city: "Kraków",
-            address: "Czarnowiejska 20",
             cash_in: false,
             location: CLLocationCoordinate2D(latitude: 50.0647, longitude: 19.9450),
+            city: "Kraków",
+            address: "Czarnowiejska 20",
             theme: .buttercup
         ),
         ATM(brand: "Euronet",
-            city: "Kraków",
-            address: "Czarnowiejska 30",
             cash_in: true,
             location: CLLocationCoordinate2D(latitude: 50.0647, longitude: 19.9450),
+            city: "Kraków",
+            address: "Czarnowiejska 30",
             theme: .indigo
         )
     ]
