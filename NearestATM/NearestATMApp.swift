87e6854aa969c9b3a6e4840b6897c36270ff42fe
Piotr Wilkosz <pwilkosz99@gmail.com>
2023-12-19 09:27:12 +0100

    Tab view

--- NearestATM/NearestATMApp.swift
+++ NearestATM/NearestATMApp.swift
@@ -7,17 +7,17 @@ struct NearestATMApp: App {
         do {
             let fileURL = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
                 .appendingPathComponent("atms_krk.json")
-
+            
             let jsonData = try Data(contentsOf: fileURL)
             
             if let jsonString = String(data: jsonData, encoding: .utf8) {
-                    print("Raw JSON Data:")
-                    print(jsonString)
-                }
-
+                print("Raw JSON Data:")
+                print(jsonString)
+            }
+            
             let decoder = JSONDecoder()
             let atmArray = try decoder.decode(OSMWrapper.self, from: jsonData)
-
+            
             print(atmArray.elements)
             ATMs = atmArray.elements
             
@@ -25,15 +25,24 @@ struct NearestATMApp: App {
             print("Error reading or decoding JSON: \(error)")
         }
     }
-
+    
     @State private var ATMs = [ATM]()
     
     var body: some Scene {
         WindowGroup {
-            ATMsView(atms: $ATMs)
-                .onAppear {
-                    readAndDecodeJSON()
-                }
+            TabView {
+                ATMsView(atms: $ATMs)
+                    .tabItem {
+                        Label("List view", systemImage: "list.bullet")
+                    }
+                    .onAppear {
+                        readAndDecodeJSON()
+                    }
+                MapView()
+                    .tabItem {
+                        Label("Map view", systemImage: "map.circle.fill")
+                    }
+            }
         }
     }
 }
---
+++ NearestATM/Views/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  NearestATM
+//
+//  Created by Piotr Wilkosz on 19/12/2023.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
