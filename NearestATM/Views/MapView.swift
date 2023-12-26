853d8b9f7b866040e51aea7bdeec53d9043427f7
Piotr Wilkosz <pwilkosz99@gmail.com>
2023-12-26 01:22:05 +0100

    User location

--- NearestATM/Views/MapView.swift
+++ NearestATM/Views/MapView.swift
@@ -6,10 +6,20 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map()
+        {
+            
+        }
+        .mapControls {
+            MapUserLocationButton()
+        }
+        .onAppear {
+            CLLocationManager().requestWhenInUseAuthorization()
+        }
     }
 }
 
